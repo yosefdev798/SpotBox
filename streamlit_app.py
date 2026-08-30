9087c9172abc3d9b98c17439d88edcb44be5a57c
yosefdev798 <yosefabosalleh@gmail.com>
2026-08-30 08:13:28 +0300

    Remove use_column_width from image display

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image')
     st.write("Detecting...")
     
     results = model(image)
